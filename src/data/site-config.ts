2331ed16f90348af76a5e57374379909e4a819cc
gurleenandrobotics <163516616+gurleenandrobotics@users.noreply.github.com>
2026-05-25 16:28:57 -0400

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,4 +1,4 @@
-import avatar from '../assets/images/avatar.jpg';
+import avatar from '../assets/images/avatar.jpeg';
 import hero from '../assets/images/hero.jpg';
 import type { SiteConfig } from '../types';
 
